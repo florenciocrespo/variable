0b0156b3a4e7cec9cc05853e7cb2a3bc95eeda73
florenciocrespo <florencio.crespo@gmail.com>
2015-07-20 11:02:02 +0200

    bugs

--- models/models.js
+++ models/models.js
@@ -48,23 +48,6 @@ exports.Variable = Variable;
 
 // sequelize.sync() crea las tablas de datos definidas en el modelo
 //NT
-sequelize.sync().then(function() {
-  // success(..) ejecuta el manejador una vez creadas las tabas de la DB
-
-  Variable.count().then(function (count){
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
-     Variable.bulkCreate( 
-        [ {indicador: 'Calidad',   peso: 30, _0: 8, _30: 8.1, _50: 8.2,_60: 8.35,_80: 8.5,_100: 8.8,_120: 8.9,_150: 9},
-          {indicador: 'Insatisfechos',   peso: 20, _0: 5, _30: 4.85, _50: 4.7,_60: 4.6,_80: 4.5,_100:4.43,_120:4.36,_150:4.26},
-          {indicador: 'NDA',   peso: 15, _0: 81, _30: 83, _50: 85,_60: 87,_80: 95,_100:96,_120:97,_150:98},
-          {indicador: 'NDS',   peso: 10, _0: 63, _30: 65, _50: 67,_60: 75,_80: 84,_100:85,_120:86,_150:88},
-          {indicador: 'FCR',   peso: 15, _0: 72, _30: 74, _50: 75,_60: 76,_80: 77,_100:78,_120:80,_150:83},
-          {indicador: 'Av/Boletines',   peso: 10, _0: 36, _30: 34, _50: 32,_60: 30,_80: 28,_100:26,_120:24,_150:22}
-        ]        
-      ).then(function(){console.log('Base de datos inicializada .')});
-    };
-  });//fin vnt
-});
 
 // cat BA
 sequelize.sync().then(function() {
@@ -83,4 +66,23 @@ sequelize.sync().then(function() {
     };
   });//fin variable
 
+});
+
+
+sequelize.sync().then(function() {
+  // success(..) ejecuta el manejador una vez creadas las tabas de la DB
+
+  Variable.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+     Variable.bulkCreate( 
+        [ {indicador: 'Calidad',   peso: 30, _0: 8, _30: 8.1, _50: 8.2,_60: 8.35,_80: 8.5,_100: 8.8,_120: 8.9,_150: 9},
+          {indicador: 'Insatisfechos',   peso: 20, _0: 5, _30: 4.85, _50: 4.7,_60: 4.6,_80: 4.5,_100:4.43,_120:4.36,_150:4.26},
+          {indicador: 'NDA',   peso: 15, _0: 81, _30: 83, _50: 85,_60: 87,_80: 95,_100:96,_120:97,_150:98},
+          {indicador: 'NDS',   peso: 10, _0: 63, _30: 65, _50: 67,_60: 75,_80: 84,_100:85,_120:86,_150:88},
+          {indicador: 'FCR',   peso: 15, _0: 72, _30: 74, _50: 75,_60: 76,_80: 77,_100:78,_120:80,_150:83},
+          {indicador: 'Av/Boletines',   peso: 10, _0: 36, _30: 34, _50: 32,_60: 30,_80: 28,_100:26,_120:24,_150:22}
+        ]        
+      ).then(function(){console.log('Base de datos inicializada .')});
+    };
+  });//fin vnt
 });
